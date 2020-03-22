4c88440a8394eb527e73df4680c09fc17570a950
Taylor Cooke <taylorcooke@gmail.com>
2020-03-21 21:02:42 -0400

    can choose role

--- trail.py
+++ trail.py
@@ -12,6 +12,27 @@ def getName():
   name = input()
   return name
 
+def setRole():
+  while True:
+    print("Who do you want to be: 1 for Doctor, 2 for Banker")
+    role = input()
+    if role == '1':
+      role = 'Doctor'
+      health = 125
+      money = 100
+      break
+    elif role == '2':
+      role = 'Banker'
+      health = 100
+      money = 150
+      break
+    else:
+      print('Please enter something valid')
+  return role, health, money
+
 
 name = getName()
 print(f"Welcome, {name}, to the adventure!")
+
+(role, health, money) = setRole()
+print(f"Role: {role}, Health: {health}, Money: {money}")
